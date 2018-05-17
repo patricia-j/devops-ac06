05e06801f79fc87e8f14c04829ca809e34777c75
patricia-j <36246275+patricia-j@users.noreply.github.com>
2018-05-17 19:33:55 -0300

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -17,6 +17,8 @@ urlpatterns = patterns('',
     url(r'^contact$', 'app.views.contact', name='contact'),
     url(r'^about', 'app.views.about', name='about'),
     url(r'^cadastro_cursos', 'app.views.cadastro_cursos', name='cadastro_cursos'),
+    url(r'^cadastro_vestibulares', 'app.views.cadastro_vestibulares’, name='cadastro_vestibulares'),
+    url(r'^lista_alunos', 'app.views.lista_alunos’, name='lista_alunos'),
     url(r'^login/$',
         'django.contrib.auth.views.login',
         {
@@ -43,5 +45,4 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 )
 
-    url(r'^cadastro_vestibulares', 'app.views.cadastro_vestibulares’, name='cadastro_vestibulares'),
-     url(r'^lista_alunos', 'app.views.lista_alunos’, name='lista_alunos'),
+
